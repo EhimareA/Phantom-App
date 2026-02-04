e397e8e1c79b1169e028e7071c6c73c71927c86f
EhimareA <65354102+EhimareA@users.noreply.github.com>
2026-02-04 06:46:01 -0600

    Update geo.py

--- geo.py
+++ geo.py
@@ -1,3 +1,19 @@
+import sqlite3
+
+# connecting to the database
+connection = sqlite3.connect("gfg.db")
+
+# cursor
+crsr = connection.cursor()
+
+# print statement will execute if there
+# are no errors
+print("Connected to the database")
+
+# close the connection
+connection.close()
+
+
 print('Hello')
 
 def main():
