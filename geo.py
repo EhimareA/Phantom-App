d5bd3df919038b931d1440fbff4299083ad13154
EhimareA <65354102+EhimareA@users.noreply.github.com>
2026-02-17 10:53:16 -0600

    Update geo.py

--- geo.py
+++ geo.py
@@ -52,11 +52,12 @@ TABLES['brands'] = (
 
 TABLES['prosthetics cost'] = (
     "CREATE TABLE `prosthetics cost` ("
-    "  `emp_no` int(11) NOT NULL,"
-    "  `dept_no` char(4) NOT NULL,"
-    "  `from_date` date NOT NULL,"
-    "  `to_date` date NOT NULL,"
-    "  PRIMARY KEY (`emp_no`,`dept_no`), KEY `emp_no` (`emp_no`),"
+    "  `tier_id` INT PRIMARY KEY IDENTITY(1,1) NOT NULL,"
+    "  `product_id` INT NOT NULL,"
+    "  `min_quantity INT NOT NULL,"
+    "  `max_quanitity` INT NULL," # NULL for above and higher
+    "  `price_per_unit` DECIMAL (10,2) NOT NULL,"
+    "  PRIMARY KEY (`tier_id`,`product_id`), KEY `emp_no` (`emp_no`),"
     "  KEY `dept_no` (`dept_no`),"
     "  CONSTRAINT `dept_emp_ibfk_1` FOREIGN KEY (`emp_no`) "
     "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,"
@@ -64,31 +65,6 @@ TABLES['prosthetics cost'] = (
     "     REFERENCES `departments` (`dept_no`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
 
-TABLES['dept_manager'] = (
-    "  CREATE TABLE `dept_manager` ("
-    "  `emp_no` int(11) NOT NULL,"
-    "  `dept_no` char(4) NOT NULL,"
-    "  `from_date` date NOT NULL,"
-    "  `to_date` date NOT NULL,"
-    "  PRIMARY KEY (`emp_no`,`dept_no`),"
-    "  KEY `emp_no` (`emp_no`),"
-    "  KEY `dept_no` (`dept_no`),"
-    "  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) "
-    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,"
-    "  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) "
-    "     REFERENCES `departments` (`dept_no`) ON DELETE CASCADE"
-    ") ENGINE=InnoDB")
-
-TABLES['titles'] = (
-    "CREATE TABLE `titles` ("
-    "  `emp_no` int(11) NOT NULL,"
-    "  `title` varchar(50) NOT NULL,"
-    "  `from_date` date NOT NULL,"
-    "  `to_date` date DEFAULT NULL,"
-    "  PRIMARY KEY (`emp_no`,`title`,`from_date`), KEY `emp_no` (`emp_no`),"
-    "  CONSTRAINT `titles_ibfk_1` FOREIGN KEY (`emp_no`)"
-    "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE"
-    ") ENGINE=InnoDB")
 
 def main():
     print("Hey! Listen")
