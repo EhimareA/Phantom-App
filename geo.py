4a41adcea53df1706fef4ae80e9b72f0371dcdc6
EhimareA <65354102+EhimareA@users.noreply.github.com>
2026-02-17 12:31:09 -0600

    Update geo.py

--- geo.py
+++ geo.py
@@ -38,11 +38,11 @@ TABLES['models'] = (
 
 TABLES['brands'] = (
     "CREATE TABLE `brands` ("
-    "  `emp_no` int(11) NOT NULL,"
-    "  `salary` int(11) NOT NULL,"
+    "  `brand_name` char(16) NOT NULL,"
+    "  `brand_no` int(11) NOT NULL,"
     "  `from_date` date NOT NULL,"
     "  `to_date` date NOT NULL,"
-    "  PRIMARY KEY (`emp_no`,`from_date`), KEY `emp_no` (`emp_no`),"
+    "  PRIMARY KEY (`brand_no`,`from_date`), KEY `brand_no` (`brand_no`),"
     "  CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`emp_no`) "
     "     REFERENCES `employees` (`emp_no`) ON DELETE CASCADE"
     ") ENGINE=InnoDB")
